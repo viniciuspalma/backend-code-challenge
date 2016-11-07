c1b15603836c14364ec085be9b7ce675ff427e8e
viniciuspalma <pockvini@gmail.com>
2016-11-07 09:59:38 -0200

    add vehicle model and enum for vehicle type

--- db/migrate/20161104154410_create_vehicles.rb
+++ db/migrate/20161104154410_create_vehicles.rb
@@ -1,7 +1,7 @@
 class CreateVehicles < ActiveRecord::Migration[5.0]
   def change
     create_table :vehicles, id: :uuid do |t|
-      t.integer :type
+      t.integer :vehicle_type
 
       t.timestamps
     end
